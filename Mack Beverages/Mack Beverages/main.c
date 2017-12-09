bef9d79f3473ab0340525f823f2a32237e82fc6f
Akampurira Mark <akamps.mark@gmail.com>
2017-12-09 20:40:18 +0300

    Code

---
+++ Mack Beverages/Mack Beverages/main.c
@@ -0,0 +1,18 @@
+/*
+ * Mack Beverages.c
+ *
+ * Created: 12/9/2017 8:39:30 PM
+ * Author : Marc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
