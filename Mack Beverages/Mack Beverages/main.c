1ae3db55b266af8ddcb899f097fb59a4dbd1df8a
Akampurira Mark <akamps.mark@gmail.com>
2017-12-18 23:46:08 +0300

    not sure

--- Mack Beverages/Mack Beverages/main.c
+++ Mack Beverages/Mack Beverages/main.c
@@ -6,14 +6,33 @@
  */ 
 
 #include <avr/io.h>
+#include <util/delay.h>
+#define rs PD0
+#define rw PD1
+#define e  PD2
 
+void latch(){
+	PORTD |=(1<<e);
+	_delay_ms(250);
+	PORTD &=~(1<<e);
+}
 
 int main(void)
 {
     /* Main coursework code*/
+	DDRD= 0xff;
+	DDRE= 0xff;
 	
-    while (1) 
-    {
-    }
+    //while (1) 
+    //{
+		PORTD &=~(1<<rs);
+		PORTE = 0xff;
+		latch();
+		PORTD |=(1<<rs);
+		PORTE = 'A';
+		latch();
+		
+		
+    //}
 }
 
